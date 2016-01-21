b8470f5121aa739cfea3306eb54e3edac898bf43
Michał Kulesza <michal.kulesza@netguru.co>
2016-01-21 17:57:45 +0100

    Recipe javadoc stub

--- examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/impl/Recipe.java
+++ examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/impl/Recipe.java
@@ -8,145 +8,188 @@ import java.io.Serializable;
 
 
 /**
- *  COPYRIGHT (C) 2016 PJATK. All Rights Reserved.
- *  Recipe class.
- *  Solves homework assignment #2
- *  @author PJATK
- *  @version 1.01 2016-01-20
+ * COPYRIGHT (C) 2016 PJATK. All Rights Reserved.
+ * Recipe class.
+ * Solves homework assignment #2
+ *
+ * @author PJATK
+ * @version 1.01 2016-01-20
  */
 public class Recipe implements Serializable {
+
+    /** name Drink's Recipe name */
     private String name;
+
+    /** price Drink's price */
     private int price;
+
+    /** amtCoffee amount of the Coffee used in Recipe */
     private int amtCoffee;
+
+    /** amtMilk amount of the Milk used in Recipe */
     private int amtMilk;
+
+    /** amtSugar amount of the Sugar used in Recipe */
     private int amtSugar;
+
+    /** amtChocolate amount of the Chocolate used in Recipe */
     private int amtChocolate;
-    
+
+    /**
+     * Constructor initializing all integer instance variables with zeroes
+     * and setting name to an empty string/
+     */
     public Recipe() {
-    	this.name = "";
-    	this.price = 0;
-    	this.amtCoffee = 0;
-    	this.amtMilk = 0;
-    	this.amtSugar = 0;
-    	this.amtChocolate = 0;
+        this.name = "";
+        this.price = 0;
+        this.amtCoffee = 0;
+        this.amtMilk = 0;
+        this.amtSugar = 0;
+        this.amtChocolate = 0;
     }
-    
+
     /**
-	 *
-	 * @return   Returns the amtChocolate.
-	 */
+     * @return Returns the amtChocolate.
+     */
     public int getAmtChocolate() {
-		return amtChocolate;
-	}
+        return amtChocolate;
+    }
+
     /**
-	 * @param amtChocolate   The amtChocolate to set.
-	 */
+     * @param amtChocolate The amtChocolate to set.
+     */
     public void setAmtChocolate(int amtChocolate) {
-		if (amtChocolate >= 0) {
-			this.amtChocolate = amtChocolate;
-		} 
-	}
+        if (amtChocolate >= 0) {
+            this.amtChocolate = amtChocolate;
+        }
+    }
+
     /**
-	 * @return   Returns the amtCoffee.
-	 */
+     * @return Returns the amtCoffee.
+     */
     public int getAmtCoffee() {
-		return amtCoffee;
-	}
+        return amtCoffee;
+    }
+
     /**
-	 * @param amtCoffee   The amtCoffee to set.
-	 */
+     * @param amtCoffee The amtCoffee to set.
+     */
     public void setAmtCoffee(int amtCoffee) {
-		if (amtCoffee >= 0) {
-			this.amtCoffee = amtCoffee;
-		} 
-	}
+        if (amtCoffee >= 0) {
+            this.amtCoffee = amtCoffee;
+        }
+    }
+
     /**
-	 * @return   Returns the amtMilk.
-	 */
+     * @return Returns the amtMilk.
+     */
     public int getAmtMilk() {
-		return amtMilk;
-	}
+        return amtMilk;
+    }
+
     /**
-	 * @param amtMilk   The amtMilk to set.
-	 */
+     * @param amtMilk The amtMilk to set.
+     */
     public void setAmtMilk(int amtMilk) {
-		if (amtMilk >= 0) {
-			this.amtMilk = amtMilk;
-		} 
-	}
+        if (amtMilk >= 0) {
+            this.amtMilk = amtMilk;
+        }
+    }
+
     /**
-	 * @return   Returns the amtSugar.
-	 */
+     * @return Returns the amtSugar.
+     */
     public int getAmtSugar() {
-		return amtSugar;
-	}
+        return amtSugar;
+    }
+
     /**
-	 * @param amtSugar   The amtSugar to set.
-	 */
+     * @param amtSugar The amtSugar to set.
+     */
     public void setAmtSugar(int amtSugar) {
-		if (amtSugar >= 0) {
-			this.amtSugar = amtSugar;
-		} 
-	}
+        if (amtSugar >= 0) {
+            this.amtSugar = amtSugar;
+        }
+    }
+
     /**
-	 * @return   Returns the name.
-	 */
+     * @return Returns the name.
+     */
     public String getName() {
-		return name;
-	}
+        return name;
+    }
+
     /**
-	 * @param name   The name to set.
-	 */
+     * @param name The name to set.
+     */
     public void setName(String name) {
-    	if(name != null) {
-    		this.name = name;
-    	}
-	}
+        if (name != null) {
+            this.name = name;
+        }
+    }
+
     /**
-	 * @return   Returns the price.
-	 */
+     * @return Returns the price.
+     */
     public int getPrice() {
-		return price;
-	}
+        return price;
+    }
+
     /**
-	 * @param price   The price to set.
-	 */
+     * @param price The price to set.
+     */
     public void setPrice(int price) {
-		if (price >= 0) {
-			this.price = price;
-		} 
-	} 
+        if (price >= 0) {
+            this.price = price;
+        }
+    }
+
+    /**
+     * @param r Recipe to compare
+     * @return Returns boolean value.
+     */
     public boolean equals(Recipe r) {
-        if((this.name).equals(r.getName())) {
+        if ((this.name).equals(r.getName())) {
             return true;
         }
         return false;
     }
+
     public String toString() {
-    	return name;
+        return name;
     }
 
-	static public Recipe getRecipe(Context context) throws ContextException {
-		Recipe r = new Recipe();
-		r.name = (String)context.getValue("name");
-		r.price = (int)context.getValue("price");
-		r.amtCoffee = (int)context.getValue("amtCoffee");
-		r.amtMilk = (int)context.getValue("amtMilk");
-		r.amtSugar = (int)context.getValue("amtSugar");
-		r.amtChocolate = (int)context.getValue("amtChocolate");
-		return r;
-	}
+    /**
+     * @param context Current context
+     * @return Created Recipe instance
+     * @throws ContextException
+     */
+    static public Recipe getRecipe(Context context) throws ContextException {
+        Recipe r = new Recipe();
+        r.name = (String) context.getValue("name");
+        r.price = (int) context.getValue("price");
+        r.amtCoffee = (int) context.getValue("amtCoffee");
+        r.amtMilk = (int) context.getValue("amtMilk");
+        r.amtSugar = (int) context.getValue("amtSugar");
+        r.amtChocolate = (int) context.getValue("amtChocolate");
+        return r;
+    }
 
-	static public Context getContext(Recipe recipe) throws ContextException {
-		Context cxt = new ServiceContext();
-		cxt.putValue("name", recipe.getName());
-		cxt.putValue("price", recipe.getPrice());
-		cxt.putValue("amtCoffee", recipe.getAmtCoffee());
-		cxt.putValue("amtMilk", recipe.getAmtMilk());
-		cxt.putValue("amtSugar", recipe.getAmtSugar());
-		cxt.putValue("amtChocolate", recipe.getAmtChocolate());
-		return cxt;
-	}
+    /**
+     * @param recipe
+     * @return
+     * @throws ContextException
+     */
+    static public Context getContext(Recipe recipe) throws ContextException {
+        Context cxt = new ServiceContext();
+        cxt.putValue("name", recipe.getName());
+        cxt.putValue("price", recipe.getPrice());
+        cxt.putValue("amtCoffee", recipe.getAmtCoffee());
+        cxt.putValue("amtMilk", recipe.getAmtMilk());
+        cxt.putValue("amtSugar", recipe.getAmtSugar());
+        cxt.putValue("amtChocolate", recipe.getAmtChocolate());
+        return cxt;
+    }
 
 
 }
