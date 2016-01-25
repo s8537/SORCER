37ed53fd8dc68d4d6d85fac09658afa39eaa36db
s13218 <ukasz.stm@gmail.com>
2016-01-25 20:54:12 +0100

    testowanie AddRecipe 1 i AddRecipe2 komentowanie pozostałych testów, które nie są potrzebne

--- examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/CoffeeMakerTest.java
+++ examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/CoffeeMakerTest.java
@@ -27,56 +27,38 @@ public class CoffeeMakerTest {
 
 	private CoffeeMaker coffeeMaker;
 	private Inventory inventory;
-	private Recipe espresso, mocha, macchiato, americano;
+	private Recipe testCoffee;
 
 	@Before
 	public void setUp() throws ContextException {
 		coffeeMaker = new CoffeeMaker();
 		inventory = coffeeMaker.checkInventory();
 
-		espresso = new Recipe();
-		espresso.setName("espresso");
-		espresso.setPrice(50);
-		espresso.setAmtCoffee(6);
-		espresso.setAmtMilk(1);
-		espresso.setAmtSugar(1);
-		espresso.setAmtChocolate(0);
-
-		mocha = new Recipe();
-		mocha.setName("mocha");
-		mocha.setPrice(100);
-		mocha.setAmtCoffee(8);
-		mocha.setAmtMilk(1);
-		mocha.setAmtSugar(1);
-		mocha.setAmtChocolate(2);
-
-		macchiato = new Recipe();
-		macchiato.setName("macchiato");
-		macchiato.setPrice(40);
-		macchiato.setAmtCoffee(7);
-		macchiato.setAmtMilk(1);
-		macchiato.setAmtSugar(2);
-		macchiato.setAmtChocolate(0);
-
-		americano = new Recipe();
-		americano.setName("americano");
-		americano.setPrice(40);
-		americano.setAmtCoffee(7);
-		americano.setAmtMilk(1);
-		americano.setAmtSugar(2);
-		americano.setAmtChocolate(0);
+		testCoffee = new Recipe();
+		testCoffee.setName("Caffee");
+		testCoffee.setPrice(50);
+		testCoffee.setAmtCoffee(6);
+		testCoffee.setAmtMilk(1);
+		testCoffee.setAmtSugar(1);
+		testCoffee.setAmtChocolate(0);
 	}
 
 	@Test
-	public void testAddRecipe() {
-		assertTrue(coffeeMaker.addRecipe(espresso));
+	public void testAddRecipe1() {
+		assertTrue(coffeeMaker.addRecipe(testCoffee));
 	}
+	@Test
+	public void testAddRecipe2() {
+		assertTrue(coffeeMaker.addRecipe(testCoffee));
+		assertTrue(coffeeMaker.addRecipe(testCoffee));
+	}
+/*
 
 	@Test
 	public void testContextCofee() throws ContextException {
-		assertTrue(espresso.getAmtCoffee() == 6);
+		assertTrue(americano.getAmtCoffee() == 7);
 	}
-
+/*
 	@Test
 	public void testContextMilk() throws ContextException {
 		assertTrue(espresso.getAmtMilk() == 1);
@@ -120,6 +102,6 @@ public class CoffeeMakerTest {
 		coffeeMaker.addRecipe(espresso);
 		assertEquals(coffeeMaker.makeCoffee(espresso, 200), 150);
 	}
-
+*/
 }
 
