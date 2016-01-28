cba3a548969772d13380683456f63df47e7818ae
Michał Kulesza <michal.kulesza@netguru.co>
2016-01-28 19:19:57 +0100

    Nextgencoffeemaker Stub

---
+++ examples/nextgencoffeemaker/src/main/java/edu/pjatk/inn/nextgencoffeemaker/impl/DeliveryImpl.java
@@ -0,0 +1,28 @@
+package edu.pjatk.inn.nextgencoffeemaker.impl;
+
+import edu.pjatk.inn.nextgencoffeemaker.Delivery;
+import sorcer.service.Context;
+import sorcer.service.ContextException;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created by Mike Sobolewski on 8/29/15.
+ */
+public class DeliveryImpl implements Delivery {
+
+    @Override
+    public Context deliver(Context context) throws RemoteException, ContextException {
+        // from/to any place the flat rate $0.60
+        Integer flatRate = 60;
+        context.putValue("delivery/cost", flatRate);
+        if (context.getValue("delivery/paid") != null) {
+            context.putValue("deliver/change", ((Integer) context.getValue("delivery/paid")) - flatRate);
+        }
+
+        if (context.getReturnPath() != null) {
+            context.setReturnValue(flatRate);
+        }
+        return context;
+    }
+}
